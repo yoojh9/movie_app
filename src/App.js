556fbd393b7ea9cf7c8a7830784c5ecfc1ca16ac
JeonghyunYoo <yoojh9@hancomm.com>
2018-11-15 15:13:23 +0900

    Loading State

--- src/App.js
+++ src/App.js
@@ -6,21 +6,6 @@ class App extends Component {
 
   // 컴포넌트가 로드 될 때 사용
   state = {
-    greeting: 'Hello',
-    movies: [
-      {
-        title:"Matrix",
-        poster:"https://images-na.ssl-images-amazon.com/images/I/51EG732BV3L._SY445_.jpg"
-      },
-      {
-        title:"star wars",
-        poster:"https://lumiere-a.akamaihd.net/v1/images/og-generic_02031d2b.png?region=0%2C0%2C1200%2C1200"
-      },
-      {
-        title:"x-man",
-        poster:"https://nyoobserver.files.wordpress.com/2018/06/59542417c9e04.jpg?quality=80&w=970"
-      }
-    ]
   }
 
   // 만약 영화 앱을 만든다면 willMount()를 진행할 때 영화 api를 호출한다.
@@ -35,24 +20,39 @@ class App extends Component {
       this.setState({
         movies: [
           {
+            title:"Matrix",
+            poster:"https://images-na.ssl-images-amazon.com/images/I/51EG732BV3L._SY445_.jpg"
+          },
+          {
+            title:"star wars",
+            poster:"https://lumiere-a.akamaihd.net/v1/images/og-generic_02031d2b.png?region=0%2C0%2C1200%2C1200"
+          },
+          {
+            title:"x-man",
+            poster:"https://nyoobserver.files.wordpress.com/2018/06/59542417c9e04.jpg?quality=80&w=970"
+          },
+          {
             title: "나는 내일, 어제의 너와 만난다",
             poster: "https://t1.daumcdn.net/cfile/tistory/9914593A5A5F45211D"
-          },
-          ...this.state.movies // 이전 리스트 출력
+          }
         ]
       })
     },2000)
   }
 
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index)=>{
+      // 리액트는 array 엘리먼트일 경우 unique key를 줘야함
+      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+    })
+    return movies
+  }
 
   render() {
     console.log('render')
     return (
       <div className="App">
-        {this.state.movies.map((movie, index)=>{
-          // 리액트는 array 엘리먼트일 경우 unique key를 줘야함
-          return <Movie title={movie.title} poster={movie.poster} key={index}/>
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
