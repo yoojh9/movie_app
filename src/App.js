b9cbef4902c484eacc411b9a06a979f0d5964a83
JeonghyunYoo <yoojh9@hancomm.com>
2018-11-15 11:15:28 +0900

    array.map() 작업 추가

--- src/App.js
+++ src/App.js
@@ -2,24 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movieTitles = [
-  "matrix",
-  "star wars",
-  "x-man"
+const movies = [
+  {
+    title:"Matrix",
+    poster:"https://images-na.ssl-images-amazon.com/images/I/51EG732BV3L._SY445_.jpg"
+  },
+  {
+    title:"star wars",
+    poster:"https://lumiere-a.akamaihd.net/v1/images/og-generic_02031d2b.png?region=0%2C0%2C1200%2C1200"
+  },
+  {
+    title:"x-man",
+    poster:"https://nyoobserver.files.wordpress.com/2018/06/59542417c9e04.jpg?quality=80&w=970"
+  }
 ]
 
-const movieImages = [
-  "https://images-na.ssl-images-amazon.com/images/I/51EG732BV3L._SY445_.jpg",
-  "https://lumiere-a.akamaihd.net/v1/images/og-generic_02031d2b.png?region=0%2C0%2C1200%2C1200",
-  "https://nyoobserver.files.wordpress.com/2018/06/59542417c9e04.jpg?quality=80&w=970"
-]
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie title={movieTitles[0]} poster={movieImages[0]}/>
-        <Movie title={movieTitles[1]} poster={movieImages[1]}/>
-        <Movie title={movieTitles[2]} poster={movieImages[2]}/>
+        {movies.map(movie=>{
+          return <Movie title={movie.title} poster={movie.poster}/>
+        })}
       </div>
     );
   }
